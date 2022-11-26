b62e7369a1615678dde4195b61c3ba69b0a0f3fb
Artemy Zhidov <71493037+ortamfag@users.noreply.github.com>
2022-11-27 02:05:35 +0300

    ADD: completeTask

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import React, {FC, ChangeEvent, useState} from 'react';
 import './App.scss'
 import TaskInput from './Components/TaskInput/TaskInput';
 import TodoTask from './Components/TodoTask/TodoTask';
+import CompleteTask from './Components/CompleteTask/CompleteTask';
 import { ITask } from './Interfaces';
 
-// interface IApp {
-//   testFunc: (e:number) => void
-// }
 
 const App: FC = () => {
 
   const [task, setTask] = useState<string>('');
   const [deadline, setDeadLine] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [completeList, setCompleteList] = useState<ITask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     let typeNameEvent = event.currentTarget.name;
@@ -31,12 +30,14 @@ const App: FC = () => {
   const addTask = (): void => {
     const newTask = { taskName: task, deadline: deadline }
     setTodoList([...todoList, newTask])
-    console.log(todoList)
     setTask("")
     setDeadLine(0)
   }
 
   const completeTask = (TaskNameToDelete: string) => {
+    const completeTask = { taskName: TaskNameToDelete, deadline: deadline }
+    setCompleteList([...completeList, completeTask])
+
     setTodoList(todoList.filter((task) => {
       return task.taskName !== TaskNameToDelete
     }))
@@ -65,16 +66,29 @@ const App: FC = () => {
         </div>
 
         <button onClick={addTask} className='addTask'>
-          <img src="./img/addTask.svg" alt="addTask"/>
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="black"/>
+          </svg>
+
           <p>Add a task</p>
         </button>
         
         <div className="todoList">
+          <h3>Tasks - {todoList.length}</h3>
           {todoList.map((task: ITask, key: number) => {
             return (
               <TodoTask key={key} task={task} completeTask={completeTask} />
             );
           })}
+        </div>
+
+        <div className='completeList'>
+        <h3>Complete tasks - {completeList.length}</h3>
+        {completeList.map((task: ITask, key: number) => {
+          return (
+            <CompleteTask key={key} task={task} />
+          )
+        })}
         </div>
       </main>
     </div>
