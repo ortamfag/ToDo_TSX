1dc337ee77f6843a54daafce98cef78ffa8a584a
ortamfag <a_zhidov@outlook.com>
2022-12-05 16:45:11 +0300

    ADD: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,62 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
         'airbnb',
-        'airbnb-typescript'
+        'airbnb-typescript',
     ],
-    "overrides": [
+    overrides: [
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": "true",
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: 'true',
         },
-        "project": ["./tsconfig.json"],
-        "tsconfigRootDir": "./",
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: './',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    plugins: [
+        'react',
+        '@typescript-eslint',
     ],
-    "rules": {
-        "indent": "off",
-        "linebreak-style": 0,
-        "@typescript-eslint/indent": "off",
-        "react/jsx-indent": "off",
-        "quotes": ['off'],
-        "no-shadow": 'off',
-        "@typescript-eslint/no-shadow": "off",
+    rules: {
+        indent: 'off',
+        'linebreak-style': 0,
+        '@typescript-eslint/indent': 'off',
+        'react/jsx-indent': 'off',
+        quotes: ['off'],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': 'off',
         'react/react-in-jsx-scope': 'off',
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
-        "react/jsx-uses-vars": "error",
-        "react/jsx-uses-react": "error", 
-        "jsx-a11y/click-events-have-key-events": "off",
-        "jsx-a11y/no-static-element-interactions": "off",
-        "jsx-a11y/no-noninteractive-element-interactions": "off",
-        "react/jsx-indent-props": 'off',
-        'max-len': ["error", { "code": 120 }],
-        "react/function-component-definition": [
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/jsx-uses-vars': 'error',
+        'react/jsx-uses-react': 'error',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/no-noninteractive-element-interactions': 'off',
+        'react/jsx-indent-props': 'off',
+        'max-len': ['error', { code: 120 }],
+        'react/function-component-definition': [
             2,
             {
-                "namedComponents": "arrow-function",
-                "unnamedComponents": "arrow-function"
-            }
-        ]
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
     },
-    "settings": {
+    settings: {
         'import/resolver': {
-            "node": {
-                "extensions": ['.js', '.jsx', '.ts', '.tsx'],
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
             },
         },
     },
-}
+};
